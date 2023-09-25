12d9f5781f232c41c9fb7a5700658543b8eb5fad
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-09-24 23:53:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Calculadora from './components/Calculadora'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>Calculadora IMC</h1>
+    <Calculadora />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Calculadora/index.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+import styles from './Calculadora.module.css'
+
+const Calculadora = () => {
+
+    const [height, setHeight] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [result, setResult] = useState(0);
+    const [rating, setRating] = useState('');
+
+    useEffect(() => {
+        if(height !== 0 || weight !== 0) {
+            const imc = (weight / (height * height)).toFixed(2);
+            setResult(imc);
+        }
+    }, [height, weight])
+
+    useEffect(() => {
+        if(result < 18.5 && result > 0) {
+            setRating('Você está em grau de magreza!');
+        } else if(result >= 18.5 && result < 24.9) {
+            setRating('Você está em niveis normais.');
+        } else if(result >= 24.9 && result < 30) {
+            setRating('Você está em sobrepeso!');
+        } else if(result >= 30) {
+            setRating('Você está em grau de obesidade!');
+        }
+
+    }, [result])
+
+    const imcCalc = (e) => {
+        if(e.target.id === 'height') {
+            setHeight((e.target.value)/100);
+        } else {
+            setWeight(e.target.value);
+        }
+    }
+
+    return(
+        <>
+            <form className={styles.form}>
+                <div className={styles.inputs}>
+                    <input type="number" placeholder="Altura (cm)" id='height' className={styles.height} onChange={imcCalc} required />
+                    <input type="number" placeholder="Peso (kg)" id='weight' className={styles.weight} onChange={imcCalc} required />
+                </div>
+                <div className={styles.results}>
+                    <span>
+                        Seu IMC: 
+                        <br /><br /> 
+                        <b>{result}</b>
+                    </span>
+                </div>
+            </form>
+            <span className={styles.textResults}>{rating}</span>
+        </>
+    )
+}
+
+export default Calculadora;
